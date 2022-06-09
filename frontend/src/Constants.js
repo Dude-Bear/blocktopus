bcaa3015cea8dc549ffce3ce7b646e630521f43a
Bjoern <bjoern.schriewer@gmail.com>
2022-06-09 13:15:08 +0200

    changed port for development

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,9 +1,11 @@
 
-
+// urls to fetch coin data 
 const production = {
     url: 'https://blocktopus-backend.herokuapp.com/'
   };
-  const development = {
-    url: 'http://localhost:3000'
-  };
-  export const config = process.env.NODE_ENV === 'development' ? development : production;
+
+const development = {
+  url: 'http://localhost:8000'
+};
+
+export const config = process.env.NODE_ENV === 'development' ? development : production;
