6556029f8014b2f7af619f9a17aa371cab5d60cc
Bjoern <bjoern.schriewer@gmail.com>
2022-06-14 11:32:58 +0200

    get Coins from backend and add empty CoinSearch component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,15 +9,31 @@ import History from './pages/History'
 import Register from './pages/Register'
 import SignIn from './pages/SignIn'
 import Learn from './pages/Learn'
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { config } from './Constants';
 
 
 
 function App() {
+  // using different URLs for development and deployment
+  const BackendURL = config.url;
+
+  const [topCoins, setCoins] = useState([])
+
+useEffect(() => {
+    axios.get(BackendURL+"/api/coins").then((response) => {
+      setCoins(response.data);
+      console.log(response.data)
+    });
+  }, [BackendURL]);
+
   return (
     <ThemeProvider>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home topCoins={topCoins}/>} />
         <Route path='/index' element={<Index />} />
         <Route path='/Coin' element={<Coin />} />
         <Route path='/learn' element={<Learn />} />
---
+++ frontend/src/components/CoinSearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CoinSearch = ({topCoins}) => {
+  return (
+    <div>{topCoins}</div>
+  )
+}
+
+export default CoinSearch
