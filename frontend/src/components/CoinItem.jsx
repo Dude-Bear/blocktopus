e25428bc7b1189b2ed4e263c126f5345161f5b1c
Bjoern <bjoern.schriewer@gmail.com>
2022-06-14 15:03:32 +0200

    display coins and add search

---
+++ frontend/src/components/CoinItem.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const CoinItem = ({ coin }) => {
+  return (
+    <tr className="h-[80px] border-b overflow-hidden">
+      <td>{coin.market_cap_rank}</td>
+      <td>
+        <div className="flex items-center">
+          <img
+            className="w-6 mr-2 rounded-full"
+            src={coin.image}
+            alt={coin.id}
+          />
+          <p className="hidden sm:table-cell pr-6">{coin.name}</p>
+        </div>
+      </td>
+      <td>{coin.symbol.toUpperCase()}</td>
+      <td>${coin.current_price.toLocaleString()}</td>
+      <td className="w-[180px] hidden sm:table-cell">
+        {coin.price_change_percentage_7d_in_currency > 0 ? (
+          <p className="text-green-600">
+            {coin.price_change_percentage_7d_in_currency.toFixed(2)}%
+          </p>
+        ) : (
+          <p className="text-red-600">
+            {coin.price_change_percentage_7d_in_currency.toFixed(2)}%
+          </p>
+        )}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">
+        {coin.market_cap.toLocaleString()}
+      </td>
+      <td>xx.xx%</td>
+    </tr>
+  );
+};
+
+export default CoinItem;
--- frontend/src/components/CoinSearch.jsx
+++ frontend/src/components/CoinSearch.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import CoinItem from "./CoinItem";
 
-const CoinSearch = ({topCoins}) => {
+const CoinSearch = ({ topCoins }) => {
+  const [searchText, setSearchText] = useState("");
   return (
-    <div>{topCoins}</div>
-  )
-}
+    <div className="rounded-div my-4">
+      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+        <h2 className="text-1xl font-bold my-2"> </h2>
+        <form>
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl"
+            type="text"
+            placeholder="Search a coin"
+          />
+        </form>
+      </div>
 
-export default CoinSearch
+      <table className="w-full border-collapse text-center">
+        <thead>
+          <tr className="border-b">
+            <th className="px-4">#</th>
+            <th className="text-left w-[10px]">Coin</th>
+            <td> </td>
+            <th className="pr-4">Price in USD</th>
+            <th className="hidden sm:table-cell">7d</th>
+            <th className="hidden md:table-cell">Mkt Cap</th>
+            <th>Mkt Cap in %</th>
+          </tr>
+        </thead>
+        <tbody>
+          {topCoins
+            .filter((value) => {
+              if (searchText === "") {
+                return value;
+              } else {
+                return value.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              }
+            })
+            .map((coin) => (
+              <CoinItem coin={coin} key={coin.id} />
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CoinSearch;
