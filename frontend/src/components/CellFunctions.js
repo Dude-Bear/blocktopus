170cd2f6f2ce1d0a359eb7ed69c7c3bf9e92d6ba
Bjoern <bjoern.schriewer@gmail.com>
2022-06-17 11:19:04 +0200

    add table version 2

---
+++ frontend/src/components/CellFunctions.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { classNames } from './shared/Utils';
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+
+export function SelectColumnFilter({
+    column: { filterValue, setFilter, preFilteredRows, id, render },
+}) {
+    // Calculate the options for filtering
+    // using the preFilteredRows
+    const options = React.useMemo(() => {
+        const options = new Set();
+        preFilteredRows.forEach(row => {
+            options.add(row.values[id]);
+        });
+        return [...options.values()];
+    }, [id, preFilteredRows]);
+
+    // Render a multi-select box
+    return (
+        <label className="flex gap-x-2 items-baseline">
+            <span className="text-gray-700">{render("Header")}: </span>
+            <select
+                className="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                name={id}
+                id={id}
+                value={filterValue}
+                onChange={e => {
+                    setFilter(e.target.value || undefined);
+                }}
+            >
+                <option value="">All</option>
+                {options.map((option, i) => (
+                    <option key={i} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </label>
+    );
+}
+
+export function PercentChangeColor({ value }) {
+    // const status = value ? value.toLowerCase() : "unknown";
+
+    return (
+        <span
+            className={classNames(
+                "text-sm",
+                value >= 0 ? " text-green-600" : null,
+                value < 0 ? " text-red-600" : null,
+            )}
+        >
+            {value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}%
+        </span>
+    );
+}
+;
+
+export function ValueToLocalString ({value}){
+    return (
+        <span className={classNames("text-sm")}>
+            {value.toLocaleString()}
+        </span>
+    );
+}
+
+export function ValueToLocalStringWithTwoDigits ({value}){
+    return (
+        <span className={classNames("text-sm")}>
+            {value.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}
+        </span>
+    );
+}
+
+export function CoinCell({ value, column, row }) {
+    return (
+        <div className="flex items-center">
+            <div className="flex-shrink-0 h-10 w-10">
+                <img className="h-10 w-10 rounded-full" src={row.original[column.imgAccessor]} alt="" />
+            </div>
+            <div className="ml-4">
+                <div className="text-sm font-medium">{value}</div>
+                <div className="text-sm text-gray-500">{row.original[column.symbolAccessor].toUpperCase()}</div>
+            </div>
+        </div>
+    );
+}
--- frontend/src/pages/BCI.jsx
+++ frontend/src/pages/BCI.jsx
@@ -1,64 +1,18 @@
 import React, { useMemo } from "react";
-import { useTable } from "react-table";
-import { COLUMNS } from "../components/columns.js";
+import { COLUMNS } from "../components/columns2";
+import Table from "../components/Table.js";
 
 function BCI({ topCoins }) {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => topCoins, [topCoins]);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({
-      columns,
-      data,
-    });
-
   return (
-    <div className="rounded-div my-4">
-      <div className="flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
-        <h2 className="text-1xl font-bold my-2"> </h2>
-        <form>
-          <input
-            className="w-full bg-primary border border-input px-4 py-2 rounded-2xl shadow-xl"
-            type="text"
-            placeholder="Search a coin"
-          />
-        </form>
-      </div>
-      <>
-        <table
-          className="w-full border-collapse text-center"
-          {...getTableProps()}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              <tr className="border-b" {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <th {...column.getHeaderProps()}>
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row);
-              return (
-                <tr
-                  className="h-[80px] border-b overflow-hidden"
-                  {...row.getRowProps()}
-                >
-                  {row.cells.map((cell) => {
-                    return (
-                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
+    <div className="min-h-screen">
+      <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+        <div className="mt-6">
+          <Table columns={columns} data={data} />
+        </div>
+      </main>
     </div>
   );
 }
