45a341ec25bc69fd407cc1b5a079a10f9adea2f3
Bjoern <bjoern.schriewer@gmail.com>
2022-06-29 10:26:46 +0200

    adjust pydantic BaseSetting and .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,18 +1,18 @@
-# from pydantic import BaseSettings
+from pydantic import BaseSettings
 
 
-# class Settings(BaseSettings):
-#     database_hostname: "localhost"
-#     database_port: str
-#     database_password: "ghij6789"
-#     database_name: "blocktopus"
-#     database_username: "postgres"
-#     secret_key: str
-#     algorithm: str
-#     access_token_expire_minutes: int
+class Settings(BaseSettings):
+    database_hostname: str
+    # database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    # secret_key: str
+    # algorithm: str
+    # access_token_expire_minutes: int
 
-#     class Config:
-#         env_file = ".env"
+    class Config:
+        env_file = "././.env"
 
 
-# settings = Settings()
+settings = Settings()
